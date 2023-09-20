340f2cbaac7b94c77bd2684970218d1d6d9009dc
Albertino Vieira <8140346@estg.ipp.pt>
2023-09-20 12:18:06 +0100

    fix auth

--- src/components/singin/SingIn.tsx
+++ src/components/singin/SingIn.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import UserAuthForm from "../userauthform/UserAuthForm";
 
 const SingIn = () => {
+
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
