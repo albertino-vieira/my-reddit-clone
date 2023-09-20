60a1e749e4002874d841b45395ee6e7794911b7e
Albertino Vieira <8140346@estg.ipp.pt>
2023-09-20 16:14:15 +0100

    added avatar section

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,20 +1,29 @@
 import Link from "next/link";
 import { Icons } from "../icons/Icons";
 import { buttonVariants } from "../ui/Button";
+import { getAuthSession } from "../../lib/auth";
+import UserAccountNav from "../useraccountnav/UserAccountNav";
 
-const NavBar = () => {
+const NavBar = async () => {
+  const authSession = await getAuthSession();
+
   return (
     <div className="fixed top-o inset-x-0 h-fit bg-zinc-100 border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
         <Link href="/" className="flex gap-2 items-center">
-            <Icons.logo className="w-8 h-8 sm:h-6 sm:w-6" />
-            <p className="hidden text-zinc-700 text-sm font-medium md:block">
-                Nextdit
-            </p>
+          <Icons.logo className="w-8 h-8 sm:h-6 sm:w-6" />
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+            Nextdit
+          </p>
         </Link>
-        <Link href="/sing-in" className={buttonVariants()}>
-            Sing In 
-        </Link>
+
+        {authSession ? (
+          <UserAccountNav user={authSession.user}/>
+        ) : (
+          <Link href="/sing-in" className={buttonVariants()}>
+            Sing In
+          </Link>
+        )}
       </div>
     </div>
   );
---
+++ src/components/useraccountnav/UserAccountNav.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import { User } from "next-auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/DropdownMenu";
+interface UserAccountNavProps {
+  user: Pick<User, "name" | "image" | "email">;
+}
+
+const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
+  return;
+  <DropdownMenu>
+    <DropdownMenuTrigger>
+      <UserAvatar user={{name: user.name || null, image: user.image || null}} />
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>Profile</DropdownMenuItem>
+      <DropdownMenuItem>Billing</DropdownMenuItem>
+      <DropdownMenuItem>Team</DropdownMenuItem>
+      <DropdownMenuItem>Subscription</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>;
+};
+
+export default UserAccountNav;
